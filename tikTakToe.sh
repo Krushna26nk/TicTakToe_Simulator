59b23b3212ba88842da8159d10650b88559a4e94
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-03-13 10:10:31 +0530

    Added:reset the board

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -x
 echo "Welcome To TicTakToe"
+declare -a array
+
+for (( count=0; count<9; count++ ))
+do
+	array[$count]=$count
+done
+
+function displayBoard()
+{
+	for (( element=0; element<9; element+=3 ))
+	do
+		echo "${array[$element]} | ${array[$element+1]} | ${array[$element+2]}"
+		echo "---------"
+	done
+}
+
+
